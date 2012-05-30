1e907e3db73e29936f7ac9cad18b15daccaa3831
Cooper Maruyama <cooperm@susteen.com>
2012-05-30 13:07:38 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soundcrowd::Application.config.secret_token = 'cae1b9b5f195a01e37b1a73edb7745a4ff8bf2e4c2adfe00865d328a090820898da227b0e3ae0829cef42ae30b96cf36c462db144f8596426e45af233c99b662'
