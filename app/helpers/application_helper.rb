2cb5e90279502b64955758a406c7657f4d91d926
coopermaruyama <cooperm@susteen.com>
2012-10-28 06:48:43 -0700

    move avatar helper to app helper from production helper so that it can be used site-wide

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
-  
+	
+	def avatar(user)
+    if FileTest.exist?("#{Rails.root}/app/assets/images/avatars/#{user.id}.png")
+  		then image_tag "avatars/#{user.id}.png", :size => "80x80", :class => "avatar" 
+  		else image_tag "avatars/_default.png", :size => "80x80", :class => "avatar" 
+  	end
+  end
+	  
   def logo
     image_tag("logo.png", :alt => 'SoundCrowd', :class=> 'logo')
   end
--- app/helpers/productions_helper.rb
+++ app/helpers/productions_helper.rb
@@ -1,8 +1,3 @@
 module ProductionsHelper
-  def avatar(user)
-    if FileTest.exist?("#{Rails.root}/app/assets/images/avatars/#{user.id}.png")
-  		then image_tag "avatars/#{user.id}.png", :size => "80x80", :class => "avatar" 
-  		else image_tag "avatars/_default.png", :size => "80x80", :class => "avatar" 
-  	end
-  end
+  
 end
